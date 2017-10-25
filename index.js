1981e30fac7edda0855a03201b3fc51c137fbba6
Nick Hathaway <Nicholashathaway@gmail.com>
2017-10-25 14:43:52 -0600

    set up passport sessions, serialize and deserialize user, and wrote the first endpt

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 const express = require('express');
 const session = require('express-session');
+const passport = require('passport');
+const strategy = require('./strategy');
 
 const app = express();
+
+// middleware for passport sessions
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use( session({
   secret: '@nyth!ng y0u w@nT',
   resave: false,
-  saveUninitialized: false
+  saveUninitialized: true
 }));
+
+passport.use(strategy);
+
+passport.serializeUser(function(user, done) {
+  const { clientID, email, name, followers_url } = user._json;
+  done(null, { clientID, email, name, followers_url });
+})
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+})
+
+app.get('/login', passport.authenticate('auth0'));
+
 
 const port = 3000;
 app.listen( port, () => { console.log(`Server listening on port ${port}`); } );
--- strategy.js
+++ strategy.js
@@ -1,4 +1,9 @@
 const Auth0Strategy = require('passport-auth0'),
     config = require('./config')
 
-    module.exports = new Auth0Strategy(Object.assign({}, config, {callbackURL: '/login'}))
+    module.exports = new Auth0Strategy(
+      Object.assign({}, config, {callbackURL: '/login'},
+      function(accessToken, refreshToken, extraParams, profile, done) {
+        return done(null, profile);
+      }
+    ))
